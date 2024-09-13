5069d76a6ae5a14ffb7ef1dafcf079d2a81f6ae8
Maheen <161057416+Maheen2024@users.noreply.github.com>
2024-09-13 21:44:28 +0500

    script.ts

--- script.ts
+++ script.ts
@@ -2,6 +2,7 @@ document.getElementById('resumeform')?.addEventListener('submit',function(event)
     event.preventDefault();
 
     // type assertion
+    const profilePictureInput = document.getElementById('profilePicture') as HTMLInputElement
     const nameElement = document.getElementById('name') as HTMLInputElement;
     const emailElement = document.getElementById('email') as HTMLInputElement;
     const phoneElement = document.getElementById('phone') as HTMLInputElement;
@@ -9,7 +10,7 @@ document.getElementById('resumeform')?.addEventListener('submit',function(event)
     const experienceElement = document.getElementById('experience') as HTMLInputElement;
     const skillsElement = document.getElementById('skills') as HTMLInputElement;
 
-if (nameElement && emailElement && phoneElement && educationElement && experienceElement && skillsElement){
+if (profilePictureInput && nameElement && emailElement && phoneElement && educationElement && experienceElement && skillsElement){
 
     const name = nameElement.value;
     const email = emailElement.value;
@@ -18,9 +19,14 @@ if (nameElement && emailElement && phoneElement && educationElement && experienc
     const experience = experienceElement.value;
     const skills = skillsElement.value;
 
+    // picture elements
+    const profilePictureFile = profilePictureInput.files?.[0]
+    const profilePictureURL = profilePictureFile ? URL.createObjectURL(profilePictureFile) : "";
+
     // create resume output
     const resumeOutput = `
     <h2>Resume</h2>
+     ${profilePictureURL ? `<img src="${profilePictureURL}" alt="Profile Picture" class="profilePicture">` : ''}
     <p><strong>Name:</strong> ${name}</p>
     <p><strong>Email:</strong> ${email}</p>
     <p><strong>Phone Number:</strong> ${phone}</p>
